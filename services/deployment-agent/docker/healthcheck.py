772bcd76e7851148d88b4116996b2202f6f64f72
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-02-28 13:43:26 +0100

    fixed healthcheck script

--- services/deployment-agent/docker/healthcheck.py
+++ services/deployment-agent/docker/healthcheck.py
@@ -16,20 +16,23 @@ Q&A:
         - SEE https://blog.sixeyed.com/docker-healthchecks-why-not-to-use-curl-or-iwr/
 """
 
+import json
 import os
 import sys
-
 from urllib.request import urlopen
 
 SUCCESS, UNHEALTHY = 0, 1
 
 # Disabled if boots with debugger
-ok = os.environ.get("SC_BOOT_MODE").lower() == "debug"
-
-# Queries host
-ok = ok or (urlopen("{host}{baseurl}".format(
+if os.environ.get("SC_BOOT_MODE").lower() == "debug":
+    sys.exit(SUCCESS)
+else:
+    # Queries host
+    response = urlopen("{host}{baseurl}".format(
         host=sys.argv[1],
-        baseurl=os.environ.get("SIMCORE_NODE_BASEPATH", "")) # adds a base-path if defined in environ
-        ).getcode() == 200 and json.loads(response.read())["data"]["status"] != "SERVICE_FAILED")
-
-sys.exit(SUCCESS if ok else UNHEALTHY)
+        baseurl=os.environ.get("SIMCORE_NODE_BASEPATH", "")))  # adds a base-path if defined in environ
+    if response.getcode() != 200:
+        sys.exit(UNHEALTHY)
+    else:
+        data = json.loads(response.read())["data"]["status"]
+        sys.exit(SUCCESS if data != "SERVICE_FAILED" else UNHEALTHY)
