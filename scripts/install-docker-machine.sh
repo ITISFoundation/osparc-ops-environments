351a177e0d42ac78174377fbf78332097a733ee6
Pedro Crespo <crespo@itis.ethz.ch>
2019-08-29 20:53:11 +0200

    Minor

--- scripts/install-docker-machine.sh
+++ scripts/install-docker-machine.sh
@@ -11,3 +11,13 @@ curl -L https://github.com/docker/machine/releases/download/v${VERSION}/docker-m
 #  latest virtual box
 sudo add-apt-repository multiverse && sudo apt-get update
 sudo apt install -y virtualbox
+
+
+# NOTE: 
+#
+# When installed virtualbox, ``docker swarm init`` in th host will error
+#
+#   Error response from daemon: could not choose an IP address to advertise since this system has multiple addresses on different interfaces (192.***.***.*** on eno1 and 192.***.***** on vboxnet0) - specify one with --advertise-addr
+#
+# Can remove vboxnet0 as
+#   VBoxManage hostonlyif remove vboxnet0
