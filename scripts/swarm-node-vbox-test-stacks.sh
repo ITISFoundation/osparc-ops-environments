b24b5ea69c55557c0575c6a3745814ff876f5189
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-09-04 14:31:22 +0200

    added steps to setting up stacks

--- scripts/swarm-node-vbox-test-stacks.sh
+++ scripts/swarm-node-vbox-test-stacks.sh
@@ -6,18 +6,46 @@ echo "======> State of current cluster"
 docker-machine ssh manager1 "docker node ls"
 docker-machine ls
 
+# install python3
+docker-machine ssh manager1 "tce-load -iw python3.6"
+docker-machine ssh manager1 "sudo pip install --upgrade pip wheel setuptools"
+# install docker-compose
+docker-machine ssh manager1 "pip install docker-compose"
+
 # copy code inside
 echo "======> Updating the stack configs $(pwd)/../services ..."
 docker-machine scp -r $(pwd)/../services manager1:service
 
-# portaier stack
+# portainer stack
 echo "======> Deploying portainer stack ..."
 docker-machine ssh manager1 "cd service/portainer; make up"
+docker-machine ssh manager1 "docker stack ls"
+
+# minio stack
+echo "======> Deploying minio stack ..."
+# label the machines
+docker-machine ssh manager1 "docker node update --label-add minio1=true manager1"
+docker-machine ssh manager1 "docker node update --label-add minio2=true worker1"
+docker-machine ssh manager1 "docker node update --label-add minio3=true worker2"
+docker-machine ssh manager1 "docker node update --label-add minio4=true worker3"
+# configure .env to create 4 minios
+docker-machine ssh manager1 "sed -i 's/MINIO_NUM_MINIOS=1/MINIO_NUM_MINIOS=4/g' .env"
+# deploy stack
+docker-machine ssh manager1 "cd service/minio; make up"
+docker-machine ssh manager1 "docker stack ls"
+
+# logging stack
+docker-machine ssh manager1 "cd service/graylog; make up"
 docker-machine ssh manager1 "docker stack ls"
 
 # monitoring stack 
 echo "======> Deploying monitoring stack ..."
 docker-machine ssh manager1 "cd service/monitoring; make up"
+docker-machine ssh manager1 "docker stack ls"
+
+# deployer stack
+echo "======> Deploying monitoring stack ..."
+docker-machine ssh manager1 "cd service/deployment-agent; make up"
 docker-machine ssh manager1 "docker stack ls"
 
 # responsive ?
