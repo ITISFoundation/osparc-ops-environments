4e72ea76dc4cc7060c3136467c9ea66b6c3625b3
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-09-04 14:31:10 +0200

    fix setting variable in script

--- scripts/install-docker-machine.sh
+++ scripts/install-docker-machine.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # docker-machine
-VERSION ?= 0.16.1
+VERSION=0.16.2
 curl -L https://github.com/docker/machine/releases/download/v${VERSION}/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
     chmod +x /tmp/docker-machine &&
     sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
