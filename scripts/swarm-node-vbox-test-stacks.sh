6f985ed99bfcbf63dd8aecfeab89ba0421fcd5c2
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-09-08 20:42:57 +0200

    replaced .env by .env.config

--- scripts/swarm-node-vbox-test-stacks.sh
+++ scripts/swarm-node-vbox-test-stacks.sh
@@ -34,7 +34,7 @@ docker-machine ssh manager1 "docker node update --label-add minio4=true worker3"
 # configure .env to create 4 minios
 docker-machine ssh manager1 "source .venv/bin/activate; \
                             cd service/minio; \
-                            sed -i 's/MINIO_NUM_MINIOS=1/MINIO_NUM_MINIOS=4/g' .env \
+                            sed -i 's/MINIO_NUM_MINIOS=1/MINIO_NUM_MINIOS=4/g' .env.config \
                             make up"
 docker-machine ssh manager1 "docker stack ls"
 
@@ -43,11 +43,11 @@ echo "======> Deploying graylog stack ..."
 manager_ip = docker-machine ip manager1
 docker-machine ssh manager1 "source .venv/bin/activate; \
                             cd service/graylog; \
-                            sed -i 's/127.0.0.1/${manager_ip}/g' .env
+                            sed -i 's/127.0.0.1/${manager_ip}/g' .env.config
                             make up"
 docker-machine ssh manager1 "docker stack ls"
 
-# monitoring stack 
+# monitoring stack
 echo "======> Deploying monitoring stack ..."
 docker-machine ssh manager1 "cd service/monitoring; \
                             make up"
