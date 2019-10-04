416fa0531bfc190e869ce332e1bffedd08e3e59f
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-10-04 15:38:41 +0200

    completed script

--- scripts/local-deploy.sh
+++ scripts/local-deploy.sh
@@ -17,13 +17,29 @@ current_git_branch=$(git branch | grep \* | cut -d ' ' -f2)
 source ${repo_basedir}/repo.config
 source ${repo_basedir}/services/portainer/.env
 
-pushd $repo_basedir;
-echo "Deploying osparc on local system..."
+cd $repo_basedir;
+
+echo "Deploying osparc on ${MACHINE_FQDN}..."
+echo "Do you to add this name to the local host file?"
+select yn in "Yes" "No"; do
+    case $yn in
+    Yes ) \
+        echo \
+        machine_ip=$(hostname -I | cut -d' ' -f1); \
+        echo "adding ${MACHINE_FQDN} using $machine_ip"; \
+        make install-full-qualified-domain-name; \
+        break;;
+    No ) echo ""; \
+        echo "using $(hostname -f)"
+        break;;
+    esac
+done
+
 echo
 echo "Do you wish to use self-signed certificates?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes | Y | y ) \
+        Yes  ) \
             echo ""; \
             echo "creating certificates..."; \
             make create-certificates; \
@@ -39,63 +55,56 @@ select yn in "Yes" "No"; do
 
         No ) echo ""; \
             mkdir -p certificates; \
-            echo "Please copy your VALID certificates in $pwd/certificates and rename them to domain.crt/domain.key"; \
+            echo "Please copy your VALID certificates in $(pwd)/certificates and rename them to domain.crt/domain.key"; \
             echo "Please press any key when done"; \
             read -s -n 1 key; \
-            echo |
+            echo \
             echo "Did you really put the certificate there"; \
             read -s -n 1 key; \
-            exit;;
+            break;;
     esac
 done
-# start portainer
-echo
-echo starting portainer
-pushd ${repo_basedir}/services/portainer
-make up
-popd
 
-# start traefik with self-signed certificates
 echo
-echo starting traefik
+echo starting portainer...
+pushd ${repo_basedir}/services/portainer; make up; popd
+
+echo
+echo starting traefik...
 pushd ${repo_basedir}/services/traefik
-cp ${repo_basedir}/certificates/rootca.crt secrets/rootca.crt
-cp ${repo_basedir}/certificates/domain.crt secrets/domain.crt
-cp ${repo_basedir}/certificates/domain.key secrets/domain.key
+# copy certificates to traefik
+cp ${repo_basedir}/certificates/* secrets/
+# set MACHINE_FQDN
+sed -i "s/MACHINE_FQDN=.*/MACHINE_FQDN=$MACHINE_FQDN/" .env
 make up
 popd
 
 echo
-echo starting minio
-pushd ${repo_basedir}/services/minio
-make up
-popd
+echo starting minio...
+pushd ${repo_basedir}/services/minio; make up; popd
 
 echo
-echo starting portus/registry
+echo starting portus/registry...
 pushd ${repo_basedir}/services/portus
-cp ${repo_basedir}/certificates/rootca.crt secrets/rootca.crt
-cp ${repo_basedir}/certificates/domain.crt secrets/portus.crt
-cp ${repo_basedir}/certificates/domain.key secrets/portus.key
+# copy certificates to traefik
+cp ${repo_basedir}/certificates/* secrets/
+# set MACHINE_FQDN
+sed -i "s/MACHINE_FQDN=.*/MACHINE_FQDN=$MACHINE_FQDN/" .env
 make up
 popd
 
 echo
-echo starting monitoring
-pushd ${repo_basedir}/services/monitoring
-make up
-popd
+echo starting monitoring...
+# set MACHINE_FQDN
+sed -i "s/GF_SERVER_ROOT_URL=.*/GF_SERVER_ROOT_URL=https://$MACHINE_FQDN/grafana/" grafana/config.monitoring
+pushd ${repo_basedir}/services/monitoring; make up; popd
 
 echo
-echo starting graylog
-pushd ${repo_basedir}/services/graylog
-make up
-popd
+echo starting graylog...
+# set MACHINE_FQDN
+sed -i "s/GF_SERVER_ROOT_URL=.*/GF_SERVER_ROOT_URL=https://$MACHINE_FQDN/grafana/" .env
+pushd ${repo_basedir}/services/graylog; make up; popd
 
 echo
-echo starting deployment-agent/simcore
-pushd ${repo_basedir}/services/deployment-agent
-make build up
-popd
-
-popd
+echo starting deployment-agent/simcore...
+pushd ${repo_basedir}/services/deployment-agent; make build up; popd
