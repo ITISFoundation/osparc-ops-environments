ef47b0f550528ca8da34ab2bd79692ee8de796f6
sanderegg <35365065+sanderegg@users.noreply.github.com>
2019-10-18 20:47:04 +0200

    fixing unit tests

--- services/deployment-agent/tests/test_git_url_watcher.py
+++ services/deployment-agent/tests/test_git_url_watcher.py
@@ -16,7 +16,9 @@ from simcore_service_deployment_agent import git_url_watcher
 def _list_valid_configs():
     return [
         "valid_git_config.yaml",
-        "valid_git_config_path.yaml"
+        "valid_git_config_path.yaml",
+        "valid_git_config_staging.yaml",
+        "valid_git_config_staging_tags.yaml",
     ]
 
 @pytest.fixture(scope="session", params=_list_valid_configs())
@@ -25,10 +27,14 @@ def valid_git_config(here, request):
         return yaml.safe_load(fp)
 
 async def test_watcher_workflow(loop, valid_git_config, mocker):
+
     mock = mocker.patch(
         "simcore_service_deployment_agent.git_url_watcher.run_cmd_line", return_value=Future())
     mock.return_value.set_result("")
+    mock_latest_tag = mocker.patch.object(git_url_watcher, '_git_get_latest_matching_tag', return_value=Future())
+    mock_latest_tag.return_value.set_result("1.2.3")
     git_watcher = git_url_watcher.GitUrlWatcher(valid_git_config)
+
 
     with pytest.raises(AssertionError):
         await git_watcher.check_for_changes()
@@ -40,12 +46,14 @@ async def test_watcher_workflow(loop, valid_git_config, mocker):
         pytest.fail("Unexpected error cloning repos...")
     mock.assert_called()
 
-    assert await git_watcher.check_for_changes() == (False, "")
+    assert not await git_watcher.check_for_changes()
 
     mock_changed_files = mocker.patch(
         "simcore_service_deployment_agent.git_url_watcher.run_cmd_line", return_value=Future())
     mock_changed_files.return_value.set_result("Makefile")
-    assert await git_watcher.check_for_changes() == (True, "Makefile")
+    assert await git_watcher.check_for_changes() == {
+        "simcore-github-repo":f"simcore-github-repo:{valid_git_config['main']['watched_git_repositories'][0]['branch']}:Makefile"
+        }
 
     try:
         await git_watcher.cleanup()
