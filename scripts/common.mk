21e54b67fe3791a1c1b3fc004cee716ddd09018a
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2019-10-23 21:49:01 +0200

    Fix/postgres exporter (#38)
    sync posgress usr/pass in POSTGRES_EXPORTER_DATA_SOURCE_NAME
    create helpers for makefiles under scripts/*.mk

---
+++ scripts/common.mk
@@ -0,0 +1,31 @@
+#
+# Basic common targets and recipes
+#
+
+.PHONY: help
+help: ## This colourful help
+	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-20s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+
+
+.PHONY: info info-swarm
+
+info: ## displays setup information
+	# setup info:
+	@echo ' makefile dir : $(CURDIR)'
+	# tools version
+	@echo ' make         : $(shell make --version 2>&1 | head -n 1)'
+	@echo ' jq           : $(shell jq --version)'
+	@echo ' awk          : $(shell awk -W version 2>&1 | head -n 1)'
+	@echo ' python       : $(shell python3 --version)'
+
+info-swarm: ## displays info about stacks and networks
+ifneq ($(SWARM_HOSTS), )
+	# Stacks in swarm
+	@docker stack ls
+	# Containers (tasks) running in '$(SWARM_HOSTS)' stack
+	-@docker stack ps $(SWARM_HOSTS)
+	# Services in '$(SWARM_HOSTS)' stack
+	-@docker stack services $(SWARM_HOSTS)
+	# Networks
+	@docker network ls
+endif
