aad61d812ebcf09093f21908d9253411a96243e7
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2019-10-23 18:33:54 +0200

    Fixes

--- scripts/info.mk
+++ scripts/info.mk
@@ -2,20 +2,12 @@
 
 info: ## displays setup information
 	# setup info:
-	@echo ' Detected OS  : $(IS_LINUX)$(IS_OSX)$(IS_WSL)$(IS_WIN)'
 	@echo ' makefile dir : $(CURDIR)'
 	# tools version
 	@echo ' make         : $(shell make --version 2>&1 | head -n 1)'
 	@echo ' jq           : $(shell jq --version)'
 	@echo ' awk          : $(shell awk -W version 2>&1 | head -n 1)'
 	@echo ' python       : $(shell python3 --version)'
-
-info-debug: ## expands all variables and relevant info on stack
-	$(info VARIABLES ------------)
-	$(foreach v,                                                                           \
-		$(filter-out $(PREDEFINED_VARIABLES) PREDEFINED_VARIABLES, $(sort $(.VARIABLES))), \
-		$(info $(v)=$($(v)))                                                               \
-	)
 
 info-swarm: ## displays info about stacks and networks
 ifneq ($(SWARM_HOSTS), )
