f7581dce3f2049865f887aca0bec6973a5caa1d0
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2019-10-23 11:24:00 +0200

    Added missing file

---
+++ scripts/portable.sh
@@ -0,0 +1,18 @@
+# Defines a series of util functions and aliases to be used to overcome compatibility issues with Mac
+
+# Return the current machine's IP
+get_this_ip()
+{
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        echo $(ipconfig getifaddr en0)
+    else
+        echo $(hostname -I | cut -d ' ' -f1)
+    fi
+}
+
+# Set psed alias for using the GNU version of sed in Mac
+if [[ $OSTYPE == "darwin"* ]]; then
+    psed=gsed
+else
+    psed=sed
+fi
