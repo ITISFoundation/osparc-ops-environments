4e94f3d31c5dd300365001c1d591e371ca88a25f
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-01-07 09:33:05 +0100

    Bump version: 0.8.1 → 0.8.2

--- services/deployment-agent/setup.py
+++ services/deployment-agent/setup.py
@@ -20,7 +20,7 @@ test_requirements = read_reqs( here / "requirements" / "_test.txt" )
 
 setup(
     name='simcore-service-deployment-agent',
-    version='0.8.1',
+    version='0.8.2',
     description='Agent that automatically deploy services in a swarm',
     packages=find_packages(where='src'),
     package_dir={
