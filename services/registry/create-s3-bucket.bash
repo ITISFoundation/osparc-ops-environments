614dc6288154d01fe3b6dfcf2ef191177613229c
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-06-22 23:27:06 +0200

    fixed registry not starting

---
+++ services/registry/create-s3-bucket.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Deploys in local host
+#
+#
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -o errexit
+set -o nounset
+set -o pipefail
+IFS=$'\n\t'
+
+
+docker run \
+    -v /etc/ssl/certs:/etc/ssl/certs:ro \
+    --network host \
+    -e MC_HOST_local="https://${S3_ACCESS_KEY_ID}:${S3_SECRET_ACCESS_KEY}@${S3_ENDPOINT}" \
+    minio/mc mb --ignore-existing local/${S3_BUCKET} 2>/dev/null
