8c3b4eec33b1450304b6aa191bba00603976fab3
Surfict <a.albronn@gmail.com>
2020-06-26 11:55:39 +0200

    S3 bucket more generic

--- scripts/create-s3-bucket.bash
+++ scripts/create-s3-bucket.bash
@@ -10,10 +10,9 @@ set -o pipefail
 IFS=$'\n\t'
 
 create_bucket() {
-    echo "ok" && \
     docker run \
     -v /etc/ssl/certs:/etc/ssl/certs:ro \
     --network host \
     -e MC_HOST_local="https://${S3_ACCESS_KEY}:${S3_SECRET_KEY}@${S3_ENDPOINT}" \
-    minio/mc mb --ignore-existing local/$1 2>/dev/null
+    minio/mc mb --ignore-existing local/$1 
 }
