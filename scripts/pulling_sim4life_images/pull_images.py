c9c00172cb528a78334ef4d67f1846f16751c123
surfict <allexandre@itis.swiss>
2023-02-16 18:25:02 +0100

    Working script

--- scripts/pulling_sim4life_images/pull_images.py
+++ scripts/pulling_sim4life_images/pull_images.py
@@ -1,4 +1,5 @@
 import os
+import boto3
 import gitlab
 import git
 import shutil
@@ -15,12 +16,18 @@ load_dotenv()
 arg = sys.argv[1]
 
 
-stacks = {"master" : {"key": "osparc-infra/docs/deployments-hardware-and-credentials/osparc.speag.com/osparc-speag.pem", "e2e-name" : "Master" }}
+
+stacks = {"master" : {"key": "osparc-infra/docs/deployments-hardware-and-credentials/osparc.speag.com/osparc-speag.pem", "e2e-name" : "Master", "sparc-external-name": "master", "registry" : "registry.osparc-master.speag.com"},
+"aws-staging" : {"key": "osparc-infra/docs/deployments-hardware-and-credentials/staging.osparc.io/osparc-staging.pem", "e2e-name" : "AWS-Staging", "sparc-external-name": "aws-staging", "registry" : "registry.staging.osparc.io" },
+"aws-production" : {"key": "osparc-infra/docs/deployments-hardware-and-credentials/osparc.io/osparc-production.pem", "e2e-name" : "AWS-Production", "sparc-external-name": "aws-production", "registry" : "registry.osparc.io" },
+"tip" : {"key": "osparc-infra/docs/deployments-hardware-and-credentials/tip.itis.swiss/osparc-public.pem", "e2e-name" : "tip.itis.swiss", "sparc-external-name": "tip-public", "registry" : "registry.tip.itis.swiss" },
+"osparc-staging.speag.com" : {"key": "osparc-infra/docs/deployments-hardware-and-credentials/osparc.speag.com/osparc-speag.pem", "e2e-name" : "Dalco", "sparc-external-name": "dalco-staging-production", "registry" : "registry.osparc-staging.speag.com" },
+"osparc.speag.com" : {"key": "osparc-infra/docs/deployments-hardware-and-credentials/osparc.speag.com/osparc-speag.pem", "e2e-name" : "Dalco", "sparc-external-name": "dalco-staging-production", "registry" : "registry.osparc.speag.com" },}
 
 args = ["master", "aws-production", "aws-staging", "dalco-staging", "dalco-production", "tip"]
 
 
-# This function parse yaml data from e2e-ops and return and list of hosts to ssh
+# This function parse yaml data from e2e-ops and return a list of hosts to ssh
 def yaml_data_to_hosts(stack_arg):
     global stacks
     stack = stacks[stack_arg]
@@ -33,11 +40,30 @@ def yaml_data_to_hosts(stack_arg):
         for host in hosts_yaml:
             for ho in host.values():
                 hosts.append(ho)
-
     return hosts
 
 
-def ssh(hosts, arg):
+# This function parse yaml data from sparc-external file and return the versions of sim4life that needs to be pulled.
+def yaml_data_to_pulling_info(stack_arg):
+    global stacks
+    stack = stacks[stack_arg]
+    pull_images = []
+    images_names = ["ci/osparc-osparc-s4l/master/s4l-core-lite", "ci/osparc-osparc-s4l/master/sym-server-dy"] 
+    with open("sparc-external/sync-workflow.yml", 'r') as file:
+        yaml_data = yaml.load(file, Loader=yaml.FullLoader)
+        images = yaml_data["stages"]
+        for sim4life_image in images_names:
+            for image in images:
+                if image["from"]["repository"] == sim4life_image:
+                    for destinations in image["to"]:
+                        if destinations["destination"] == stack["sparc-external-name"]:
+                            tags = len(destinations["tags"])
+                            tag = destinations["tags"][tags - 1]
+                            pull_images.append(destinations["repository"] + ":" + tag)
+    return pull_images
+
+
+def ssh(hosts, images_to_pull, stack_arg):
     for host in hosts:
         host_ip = host["IP"]
         port = 22
@@ -46,7 +72,7 @@ def ssh(hosts, arg):
         
         username = host["Username"]
         global stacks
-        stack = stacks[arg]
+        stack = stacks[stack_arg]
         subprocess.run("chmod 600 " + stack["key"], shell=True)
         key_filename = stack["key"]
         command = 'nvidia-smi'
@@ -56,13 +82,10 @@ def ssh(hosts, arg):
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         private_key = paramiko.RSAKey.from_private_key_file(key_filename)
-        print(host_ip)
-        print(port)
-        print(username)
-        print(private_key)
 
         try:
             # Connect to the remote host using SSH
+            print("SSH to " + host_ip)
             ssh.connect(hostname=host_ip, port=port, username=username, pkey=private_key)
 
             # Execute the 'nvidia-smi' command on the remote host
@@ -74,9 +97,17 @@ def ssh(hosts, arg):
 
             # Check if the string is not found in the output
             if not_found_str not in output:
-                print(f"The string '{not_found_str}' was not found in the output of the command.")
+                print("Nvidia-smi is present on this hosts. We pull the images")
+                for image in images_to_pull:
+                    print("docker pull " + stack["registry"] + "/" + image )
+                    stdin, stdout, stderr = ssh.exec_command("docker pull " + stack["registry"] + "/" + image )
+                    outputerr = stderr.read().decode()
+                    outputout = stdout.read().decode()
+                    print(outputerr)
+                    print(outputout)
             else:
-                print(f"The string '{not_found_str}' was found in the output of the command.")
+                print("Nvidia-smi not present.")
+
 
         except paramiko.AuthenticationException:
             print("Authentication failed. Please verify your credentials and private key file path.")
@@ -87,6 +118,26 @@ def ssh(hosts, arg):
             ssh.close()
 
 
+# This function list aws autoscaled instance that needs to pull the image, either for aws-staging or aws-production, based on their label.
+# It returns a list of the publics ips of theses instances
+def list_aws_instances(stack_arg):
+    if stack_arg == "aws-production":
+        message = "autoscaling-osparc-production"
+    if stack_arg == "aws-staging":
+        message = "autoscaling-osparc-staging"
+
+    ec2 = boto3.client('ec2', aws_access_key_id=os.getenv('AWS_ACCESS_KEY'), aws_secret_access_key=os.getenv('AWS_SECRET_KEY'), region_name="us-east-1")
+    response = ec2.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': [message]}])
+    # Extract the public IP addresses of each instance 
+    public_ips = []
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            if "PublicIpAddress" in instance:
+                public_ips.append(instance['PublicIpAddress'])
+
+    return public_ips
+
+
 
 def clone_repos():
     # Set up the GitLab API client
@@ -95,8 +146,8 @@ def clone_repos():
     # Authenticate to the GitLab API
     gl.auth()
 
-    # Clone osparc-infra (413) and e2e-ops (525)
-    for id in [413, 525]:
+    # Clone osparc-infra (413), e2e-ops (525), and sparc-external (258)
+    for id in [413, 525, 258]:
         project_id = id
         project = gl.projects.get(project_id)
         repo_url = project.ssh_url_to_repo
@@ -104,14 +155,26 @@ def clone_repos():
         git.Repo.clone_from(repo_url, repo_path)
 
 def clean_repos():
-    for repos in ["osparc-infra", "e2e-ops"]:
+    for repos in ["osparc-infra", "e2e-ops", "sparc-external" ]:
         shutil.rmtree(os.path.join(os.path.dirname(__file__), repos), ignore_errors=True)
 
-clean_repos()
-clone_repos()
+
+# Return a list of the hosts to ssh for this stack
+def hosts_list(stack_arg):
+    hosts = yaml_data_to_hosts(arg)
+    if stack_arg == "aws-production" or stack_arg == "aws-staging":
+        aws_hosts = list_aws_instances(stack_arg)
+        for host in aws_hosts:
+            hosts.append({'IP': host, 'Username': hosts[0]["Username"], 'IdentityFile': hosts[0]["IdentityFile"], 'SudoPassword': hosts[0]["SudoPassword"]})
+    return hosts
+    
+
+#clean_repos()
+#clone_repos()
 
 if arg not in args :
     print("Invalid argument. Please specify " + args)
 else:
-    hosts = yaml_data_to_hosts(arg)
-    ssh(hosts, arg)
+    hosts = hosts_list(arg)
+    images_to_pull = yaml_data_to_pulling_info(arg)
+    ssh(hosts, images_to_pull, arg)
