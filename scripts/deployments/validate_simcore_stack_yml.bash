f092fae8e01a8ca63cf6b547950eedd5f4d3c5c8
kaiser <kaiser@itis.swiss>
2023-11-15 17:00:27 +0100

    Add version-pinned yq

--- scripts/deployments/validate_simcore_stack_yml.bash
+++ scripts/deployments/validate_simcore_stack_yml.bash
@@ -6,7 +6,13 @@ export COMPOSE_FILE=simcore_stack.yml
 export SETTINGS_BINARY_PATH=/home/scu/.venv/bin
 export SERVICES_PREFIX=${PREFIX_STACK_NAME}
 exit_code=0
-for service in $(yq e '.services | keys | .[]' ${COMPOSE_FILE})
+# Download version-pinned yq binary
+python -c "import urllib.request,os,sys,urllib; f = open(os.path.basename(sys.argv[1]), 'wb'); f.write(urllib.request.urlopen(sys.argv[1]).read()); f.close();" https://github.com/mikefarah/yq/releases/download/v4.29.2/yq_linux_amd64
+mv yq_linux_amd64 yq
+chmod +x yq
+_yq=$(realpath ./yq)
+# start
+for service in $(_yq e '.services | keys | .[]' ${COMPOSE_FILE})
 do
     export TARGETNAME=${service#"${SERVICES_PREFIX}"_}
     #  continue if the service == director since it doesnt have settings
