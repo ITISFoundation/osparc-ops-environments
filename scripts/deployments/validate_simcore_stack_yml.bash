6c085c0045ed4dd26df32029eaef3b65b14523f7
kaiser <kaiser@itis.swiss>
2023-11-17 21:10:54 +0100

    Revert "Minor: Verbose output for debugging"
    This reverts commit b794623cc258ae27f0bbb1c7639a24257b3e3f23.

--- scripts/deployments/validate_simcore_stack_yml.bash
+++ scripts/deployments/validate_simcore_stack_yml.bash
@@ -51,7 +51,7 @@ do
     echo TARGETFILE="${TARGETFILE}"
     echo "Assuming targetfile in ${SETTINGS_BINARY_PATH}/${TARGETFILE}"
     echo "Checking ${SETTINGS_BINARY_PATH}/${TARGETFILE}"
-    if docker compose --file ${COMPOSE_FILE} run --rm "${service}" test -f "${SETTINGS_BINARY_PATH}"/"${TARGETFILE}"; then
+    if docker compose --file ${COMPOSE_FILE} run --rm "${service}" test -f "${SETTINGS_BINARY_PATH}"/"${TARGETFILE}" > /dev/null 2>&1; then
         echo "FOUND_EXECUTABLE=${SETTINGS_BINARY_PATH}/$TARGETFILE"
         export FOUND_EXECUTABLE="${SETTINGS_BINARY_PATH}/$TARGETFILE"
         if docker compose --file ${COMPOSE_FILE} run --entrypoint "${FOUND_EXECUTABLE}" --rm "${service}" settings --as-json > /dev/null 2>&1; then
