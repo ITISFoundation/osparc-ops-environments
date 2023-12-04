ac803d8aabe9868061887d6cf862f391cf4380cb
YuryHrytsuk <yuryhrytsuk@gmail.com>
2023-12-04 16:32:58 +0100

    dyn cfg for master

---
+++ services/traefik/scripts/gen_dyn_cfg_from_cert_domains.py
@@ -0,0 +1,45 @@
+import json
+import sys
+from pathlib import Path
+
+import yaml
+
+if len(sys.argv) != 3:
+    print(
+        "Usage: gen_dyn_cfg_from_cert_domains.py <cert_domains.json> <output_file.yml>"
+    )
+    exit(1)
+
+CERT_DOMAINS_FILE = sys.argv[1]
+OUTPUT_FILE = sys.argv[2]
+CERT_PATH_PREFIX = Path("/etc/traefik_certs")
+
+
+def main():
+    with open(CERT_DOMAINS_FILE) as f:
+        cert_domains = json.load(f)
+
+    main_domains = [item["domain"] for item in cert_domains]
+
+    for ix in range(len(main_domains)):
+        if main_domains[ix].startswith("*."):
+            main_domains[ix] = main_domains[ix].replace("*.", "_.")
+
+    dyn_cfg = {
+        "tls": {
+            "certificates": [
+                {
+                    "certFile": str(CERT_PATH_PREFIX / f"{domain}.crt"),
+                    "keyFile": str(CERT_PATH_PREFIX / f"{domain}.key"),
+                }
+                for domain in main_domains
+            ]
+        }
+    }
+
+    with open(OUTPUT_FILE, "w") as f:
+        yaml.dump(dyn_cfg, f)
+
+
+if __name__ == "__main__":
+    main()
