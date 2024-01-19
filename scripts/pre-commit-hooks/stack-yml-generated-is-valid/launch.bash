1fa09686cec5c3c3ba53d4e476fb8f0f3af2eb4c
YuryHrytsuk <yuryhrytsuk@gmail.com>
2024-01-19 13:59:56 +0100

    review fixes

--- scripts/pre-commit-hooks/stack-yml-generated-is-valid/launch.bash
+++ scripts/pre-commit-hooks/stack-yml-generated-is-valid/launch.bash
@@ -51,14 +51,10 @@ rm "$repo_basedir"/docker-compose.yml 2>/dev/null || true
 rm "$repo_basedir"/.env-devel 2>/dev/null || true
 # via https://stackoverflow.com/a/2466755
 rm -rf osparc-simcore 2>/dev/null || true
-git clone https://github.com/ITISFoundation/osparc-simcore
+git clone --depth=1 https://github.com/ITISFoundation/osparc-simcore
 pushd osparc-simcore
-# shellcheck disable=2001
-latestMatchingTag=HEAD
 
-echo Checking out "$latestMatchingTag" on osparc-simcore
-git checkout "$latestMatchingTag" services/docker-compose.yml
-git checkout "$latestMatchingTag" .env-devel
+# shellcheck disable=2001
 cp services/docker-compose.yml "$repo_basedir"
 cp .env-devel "$repo_basedir"
 popd
