fbe3eed40729daf6f93b519435d92d9f79ff0bdd
Dustin Kaiser <8209087+mrnicegyu11@users.noreply.github.com>
2024-03-19 12:24:26 +0100

    Fix: Use `docker stack config` (#610)
    * fix
    
    * Deprecate docker compose v1
    
    * use docker stack config
    
    ---------
    
    Co-authored-by: Dustin Kaiser <mail@dustinkaiser.eu>
    Co-authored-by: kaiser <kaiser@itis.swiss>

--- scripts/docker-compose-config.bash
+++ scripts/docker-compose-config.bash
@@ -57,19 +57,20 @@ if docker compose version --short | grep --quiet "^2\." ; then
   if [[ -z "$version" ]]; then
     version="3.9"  # Default to 3.9 if version is not found in file
   fi
-
+# shellcheck disable=SC2002
   docker_command="\
-docker \
---log-level=ERROR \
-compose \
---env-file ${env_file}"
+set -o allexport && \
+export $(cat "${env_file}" | sort) && set +o allexport && \
+docker stack config"
 
   for compose_file_path in "$@"
   do
-    docker_command+=" --file=${compose_file_path}"
+    docker_command+=" --compose-file ${compose_file_path}"
   done
-  docker_command+="\
- config \
+
+  #docker_command+=" --skip-interpolation"
+
+  docker_command+=" \
 | sed '/published:/s/\"//g' \
 | sed '/size:/s/\"//g' \
 | sed '1 { /name:.*/d ; }' \
@@ -80,25 +81,6 @@ compose \
   show_info "Executing Docker command: ${docker_command}"
   eval "${docker_command}"
 else
-  show_warning "docker compose V2 is not available, trying V1 instead... please update your docker engine."
-  if docker-compose version --short | grep --quiet "^1\." ; then
-    show_info "Running compose V1"
-    docker_command="\
-docker-compose \
---log-level=ERROR \
---env-file ${env_file}"
-    for compose_file_path in "$@"
-    do
-      docker_command+=" --file=${compose_file_path} "
-    done
-    docker_command+=" \
-config \
-| sed --regexp-extended 's/cpus: ([0-9\\.]+)/cpus: \"\\1\"/'"
-    # Execute the command
-    show_info "Executing Docker command: ${docker_command}"
-    eval "${docker_command}"
-  else
-    show_error "docker-compose V1 is not available. It is impossible to run this script!"
-    exit 1
-  fi
+  show_warning "docker compose V2 is not available... please update your docker engine."
+  exit 1
 fi
