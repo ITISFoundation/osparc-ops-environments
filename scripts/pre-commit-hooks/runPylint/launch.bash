2026bfeb689af5033e221351de8118be51b3970a
YuryHrytsuk <yuryhrytsuk@gmail.com>
2024-03-27 16:28:54 +0100

    fixes

--- scripts/pre-commit-hooks/runPylint/launch.bash
+++ scripts/pre-commit-hooks/runPylint/launch.bash
@@ -7,14 +7,14 @@ IFS=$'\n\t'
 repo_basedir=$(git rev-parse --show-toplevel)
 script_dir=$(dirname "$0")
 
-if [ ! -f ".venv/bin/activate" ] ; then
+if [ ! -f "$script_dir/.venv/bin/activate" ] ; then
     echo "Installing and creating venv.."
-    pip3 install virtualenv >/dev/null 2>&1
-    virtualenv -p python3 .venv >/dev/null 2>&1
+    pip3 install virtualenv
+    virtualenv -p python3 "$script_dir"/.venv
 fi
 
 # shellcheck disable=1091,1090
-source .venv/bin/activate
+source "$script_dir"/.venv/bin/activate
 python -m pip install -r "$script_dir"/requirements.txt
 
 pylint --rcfile="$repo_basedir"/.pylintrc "$repo_basedir"
