e3b22a8ce218026158ba0726e4fff6f97071e912
Dustin Kaiser <8209087+mrnicegyu11@users.noreply.github.com>
2024-05-27 13:19:36 +0200

    Address makefile issues (#681)
    Co-authored-by: Dustin Kaiser <mail@dustinkaiser.eu>

--- scripts/portable.sh
+++ scripts/portable.sh
@@ -3,12 +3,16 @@
 # Defines a series of util functions and aliases to be used to overcome compatibility issues with Mac
 
 # Return the current machine's IP
+# Don't use `hostname -I`. `hostname` is part of net-tools, a package universally regarded as old and unmaintained.
+# Modern GNU/Linux distributions use the up-to-date implementation from GNU/inetutils
+# However, debian contributor Miachael Meskes has decided to code a dedicated, different `hostname` for debian, which follows different versioning schemes and
+# is incompatible with the "official" one from GNU. The CLI option `-I` is not supported by the GNU/inetutils's hostname.
 get_this_private_ip()
 {
     if [[ "$OSTYPE" == "darwin"* ]]; then
         ipconfig getifaddr en0
     else
-        hostname -I | cut -d ' ' -f1
+        hostname -i | cut -d ' ' -f1
     fi
 }
 
