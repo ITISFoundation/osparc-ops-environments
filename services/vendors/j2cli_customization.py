f496df61391e0c0d5fada29a973f5921179d07ee
YuryHrytsuk <yuryhrytsuk@gmail.com>
2024-11-21 09:57:32 +0100

    Fix traefik rule

--- services/vendors/j2cli_customization.py
+++ services/vendors/j2cli_customization.py
@@ -1,7 +1,7 @@
 def _generate_vendors_manual_traefik_rule(domains: str, subdomain_prefix: str) -> str:
     domain_list = domains.strip().strip(",").split(",")
     domains = [f"{subdomain_prefix}.{domain}" for domain in domain_list]
-    return " || ".join(domains)
+    return " || ".join(f"Host(`{d}`)" for d in domains)
 
 
 def j2_environment(env):
