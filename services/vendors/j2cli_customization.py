c0e11cd19fb73b0434b2a0e7a4fb6083371e8a3a
Yury Hrytsuk <50014626+YuryHrytsuk@users.noreply.github.com>
2024-11-21 14:46:41 +0100

    vendors stack: support multiple domains & maintenance mode (#886)
    * vendors: support multiple domains + maintenance mode
    
    * include maintenance
    
    * Fix traefik rule

---
+++ services/vendors/j2cli_customization.py
@@ -0,0 +1,11 @@
+def _generate_vendors_manual_traefik_rule(domains: str, subdomain_prefix: str) -> str:
+    domain_list = domains.strip().strip(",").split(",")
+    domains = [f"{subdomain_prefix}.{domain}" for domain in domain_list]
+    return " || ".join(f"Host(`{d}`)" for d in domains)
+
+
+def j2_environment(env):
+    env.globals.update(
+        generate_vendors_manual_traefik_rule=_generate_vendors_manual_traefik_rule
+    )
+    return env
