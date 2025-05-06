c8f98aa4a4cca0c3d3796a237d80db96e6ad5511
Dustin Kaiser <mrnicegyu11@users.noreply.github.com>
2025-05-06 13:23:21 +0200

    Add graylog system notification cleaner - 4

--- services/graylog/scripts/clean_system_notifications.py
+++ services/graylog/scripts/clean_system_notifications.py
@@ -32,7 +32,7 @@ def delete_notification(
     url = f"{base_url}/api/system/notifications/{notification_type}/{notification_id}"
 
     headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:138.0) Gecko/20100101 Firefox/138.0",
+        "User-Agent": "cli",
         "Accept": "application/json",
         "Accept-Language": "en-US,en;q=0.5",
         "X-Requested-With": "XMLHttpRequest",
